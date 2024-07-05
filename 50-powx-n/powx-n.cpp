65c6dcbac0784c7cbb32ff361d7853c737395f2a
Muhammad Junaid Raza <114643027+MalikJunaid92@users.noreply.github.com>
2024-07-05 21:35:25 +0500

    Time: 0 ms (100.00%) | Memory: 7.8 MB (93.43%) - LeetSync

---
+++ 50-powx-n/powx-n.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        if (n == 0)
+            return 1;
+        if (x == 0)
+            return 0;
+        long long N = n;
+        if (N < 0) {
+            x = 1 / x;
+            N = -N;
+        }
+        return (N % 2 == 0) ? myPow(x * x, N / 2) : x * myPow(x * x, N / 2);
+    }
+};

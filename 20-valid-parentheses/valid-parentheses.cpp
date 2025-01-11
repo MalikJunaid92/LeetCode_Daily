2afc8ec71c1962fe10dcd791758748ac6425b9f7
Muhammad Junaid Raza <114643027+MalikJunaid92@users.noreply.github.com>
2025-01-11 14:51:36 +0500

    Time: 0 ms (100.00%) | Memory: 8.6 MB (65.23%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.cpp
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -1,25 +1,22 @@
 class Solution {
 public:
     bool isValid(string s) {
-        stack<char> parenthesesStack;
-
+        stack<char> para;
         for (char c : s) {
             if (c == '(' || c == '{' || c == '[') {
-                parenthesesStack.push(c);
+                para.push(c);
             } else {
-                if (parenthesesStack.empty()) {
+                if (para.empty()) {
                     return false;
                 }
-
-                char top = parenthesesStack.top();
-                parenthesesStack.pop();
-
-                if ((c == ')' && top != '(') || (c == '}' && top != '{') || (c == ']' && top != '[')) {
+                char top = para.top();
+                para.pop();
+                if ((c == ')' && top != '(') || (c == '}' && top != '{') ||
+                    (c == ']' && top != '[')) {
                     return false;
                 }
             }
         }
-
-        return parenthesesStack.empty();
+        return para.empty();
     }
 };

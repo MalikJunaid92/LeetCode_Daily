3326ca85044b1de16e3a391d7549e405af01e4b4
Muhammad Junaid Raza <114643027+MalikJunaid92@users.noreply.github.com>
2025-01-11 15:15:07 +0500

    Time: 619 ms (5.12%) | Memory: 16.8 MB (69.67%) - LeetSync

--- 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.cpp
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.cpp
@@ -1,31 +1,24 @@
-
 class Solution {
 
-    long resolves(int a, int b, char Operator){
-        if(Operator == '+') return a + b;
-        else if(Operator == '-') return a - b;
-        else if(Operator == '*') return (long)a*b;
-        return a/b;
-    }
 public:
     int evalRPN(vector<string>& tokens) {
-        stack<long> Stack;
-        int n = tokens.size();
-        for(int i = 0; i < n; i++){
-
-            if(tokens[i].size() == 1 and tokens[i][0] < 48){
-                long integer2 = Stack.top();
-                Stack.pop();
-                long integer1 = Stack.top();
-                Stack.pop();
-                
-                string Operator = tokens[i];
-                long resolvedAns = resolves(integer1, integer2 , Operator[0]);
-                Stack.push(resolvedAns);
-            }else 
-                Stack.push(stol(tokens[i]));
+     while(tokens.size()>1){
+        for(int i=0;i<tokens.size();i++){
+            if(tokens[i]=="+" ||tokens[i]=="-" || tokens[i]=="*" || tokens[i]=="/"){
+                int a= stoi(tokens[i-2]);
+                int b= stoi(tokens[i-1]);
+                int res=0;
+                if(tokens[i] =="+") res=a+b;
+                else if(tokens[i] =="-") res=a-b;
+                else if(tokens[i] =="*") res=a*b;
+                else if(tokens[i] =="/") res=a/b;
+                tokens.erase(tokens.begin()+ i -2, tokens.begin() +i +1);
+                tokens.insert(tokens.begin()+ i -2,to_string(res));
+                break;
+            }
         }
-        return Stack.top();
+     }
+     return stoi(tokens[0]);
     }
 };
 

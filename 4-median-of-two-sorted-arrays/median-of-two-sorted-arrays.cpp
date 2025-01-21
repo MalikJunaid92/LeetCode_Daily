a87d23d18ddaca34db502fb2784c11d4057ca15c
Muhammad Junaid Raza <114643027+MalikJunaid92@users.noreply.github.com>
2025-01-21 20:59:32 +0500

    Time: 6 ms (14.53%) | Memory: 95.9 MB (27.37%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
@@ -1,23 +1,19 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        // Combine the two vectors
-        vector<int> array;
-        array.reserve(nums1.size() + nums2.size());
-        array.insert(array.end(), nums1.begin(), nums1.end());
-        array.insert(array.end(), nums2.begin(), nums2.end());
-        
-        // Sort the combined vector
-        sort(array.begin(), array.end());
-        
-        // Find the median
-        int n = array.size();
+        vector<int> nums;
+
+        nums.insert(nums.end(), nums1.begin(), nums1.end());
+        nums.insert(nums.end(), nums2.begin(), nums2.end());
+
+        sort(nums.begin(), nums.end());
+
+        int n = nums.size();
+
         if (n % 2 == 0) {
-            // If even, the median is the average of the two middle elements
-            return (array[n/2 - 1] + array[n/2]) / 2.0;
+            return (nums[n / 2 - 1] + nums[n / 2]) / 2.0;
         } else {
-            // If odd, the median is the middle element
-            return array[n/2];
+            return nums[n / 2];
         }
     }
 };

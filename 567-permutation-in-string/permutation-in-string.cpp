49d04bbbfeff310be8a4177c08a8a41f4291f968
Muhammad Junaid Raza <114643027+MalikJunaid92@users.noreply.github.com>
2025-02-03 13:13:13 +0500

    Time: 0 ms (100.00%) | Memory: 10.1 MB (43.44%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.cpp
+++ 567-permutation-in-string/permutation-in-string.cpp
@@ -1,30 +1,21 @@
 class Solution {
-    bool areVectorsEqual(vector<int> a, vector<int> b) {
-        for(int i = 0; i < 26; i++) {
-            if(a[i] != b[i]) return false;
-        }
-        return true;
-    }
 public:
     bool checkInclusion(string s1, string s2) {
-        if(s2.size() < s1.size()) return false;
-        vector<int> freqS1(26, 0);
-        for(char c : s1) freqS1[c - 'a']++;
-        vector<int> freqS2(26, 0);
-        int i = 0, j = 0;
+      int n=s1.size(),m=s2.size();
+      if(n > m) return false;
+      vector<int> s1freq(26,0) , s2freq(26,0);
+      for(int i=0; i < n; i++){
+        s1freq[s1[i] - 'a']++;
+        s2freq[s2[i] - 'a']++;
+      }
+      if(s1freq==s2freq) return true;
 
-        while(j < s2.size()) {
-            freqS2[s2[j] - 'a']++;
-            if(j - i + 1 == s1.size()) {
-                if(areVectorsEqual(freqS1, freqS2)) return true;
-            }
-            if(j - i + 1 < s1.size()) j++;
-            else {
-                freqS2[s2[i] - 'a']--;
-                i++;
-                j++;
-            }
-        }
-        return false;  
+      for(int i=n; i< m; i++){
+        s2freq[s2[i]-'a']++;
+        s2freq[s2[i-n]-'a']--;
+      if(s1freq==s2freq) return true;
+        
+      } 
+      return false;
     }
 };

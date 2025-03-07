215d9df02d3d6872ca08432e5d3ab8446b8c4ce5
Muhammad Junaid Raza <114643027+MalikJunaid92@users.noreply.github.com>
2025-03-07 15:21:36 +0500

    Time: 0 ms (100.00%) | Memory: 15.2 MB (26.79%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.cpp
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.cpp
@@ -1,38 +1,25 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
     vector<int> rightSideView(TreeNode* root) {
+        if (root == NULL)
+            return {};
         vector<int> res;
-        if (!root) return res;
-
-        queue<TreeNode*> q;
-        q.push(root);
-
-        while (!q.empty()) {
-            int size = q.size();
-            for (int i = 0; i < size; i++) {
-                TreeNode* node = q.front();
-                q.pop();
-                
-                // If it's the rightmost element of this level
-                if (i == size - 1) {
-                    res.push_back(node->val);
+        queue<TreeNode*> que;
+        que.push(root);
+        while (!que.empty()) {
+            int n = que.size();
+            TreeNode* node = NULL;
+            while (n--) {
+                node = que.front();
+                que.pop();
+                if (node->left != NULL) {
+                    que.push(node->left);
                 }
-                
-                // Push left and right children if they exist
-                if (node->left) q.push(node->left);
-                if (node->right) q.push(node->right);
+                if (node->right != NULL) {
+                    que.push(node->right);
+                }
             }
+            res.push_back(node->val);
         }
         return res;
     }

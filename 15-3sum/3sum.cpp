5b8bed652c4d97d1bb885daf152a56fe282d879d
Muhammad Junaid Raza <114643027+MalikJunaid92@users.noreply.github.com>
2025-08-06 08:37:33 +0500

    Time: 51 ms (51.05%) | Memory: 29.2 MB (49.28%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -1,28 +1,38 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-      sort(nums.begin(),nums.end());
-      vector<vector<int>>res;
-      for(int i=0;i<nums.size();i++){
-        if(nums[i] > 0) break;
-        if(i>0 && nums[i]==nums[i-1]) continue;
-        int l= i+1, r=nums.size()-1;
-        while(l < r){
-            int sum= nums[i] + nums[l] + nums[r];
-            if(sum > 0){
-                r--;
-            }else if(sum < 0){
-                l++;
-            }else{
-                res.push_back({nums[i],nums[l],nums[r]});
-                l++;
-                r--;
-                while(l < r && nums[l] == nums[l-1]){
+        sort(nums.begin(), nums.end());
+        int n = nums.size();
+        vector<vector<int>> res;
+
+        for (int i = 0; i < n - 2; i++) {
+            // Skip duplicate i values
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+
+            int l = i + 1;
+            int r = n - 1;
+
+            while (l < r) {
+                int sum = nums[i] + nums[l] + nums[r];
+                if (sum == 0) {
+                    res.push_back({nums[i], nums[l], nums[r]});
+
+                    // Skip duplicate l values
+                    while (l < r && nums[l] == nums[l + 1]) l++;
+                    // Skip duplicate r values
+                    while (l < r && nums[r] == nums[r - 1]) r--;
+
                     l++;
+                    r--;
+                } 
+                else if (sum < 0) {
+                    l++;
+                } 
+                else {
+                    r--;
                 }
             }
         }
-      }
-      return res;
+        return res;
     }
 };

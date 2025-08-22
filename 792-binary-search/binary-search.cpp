ff1e162f525b4144e1696bd30ca862940de61a9f
Muhammad Junaid Raza <114643027+MalikJunaid92@users.noreply.github.com>
2025-08-22 16:24:55 +0500

    Time: 0 ms (100.00%) | Memory: 31.3 MB (79.89%) - LeetSync

--- 792-binary-search/binary-search.cpp
+++ 792-binary-search/binary-search.cpp
@@ -1,18 +1,11 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        int l = 0;
-        int r = nums.size() - 1;
-        while (l <= r) {
-            int mid = (l+ r) / 2;
-            if (nums[mid] == target) {
-                return mid;  
-            } else if (nums[mid] > target) {
-                r = mid - 1;
-            } else {
-                l = mid + 1;
+        for (int i = 0; i < nums.size(); i++) {
+            if (target == nums[i]) {
+                return i;
             }
         }
-        return -1;  
+        return -1;
     }
 };

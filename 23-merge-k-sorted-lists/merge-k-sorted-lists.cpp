ac931182dc8a0b2d11162d2deac8dc766bd3abb4
Muhammad Junaid Raza <114643027+MalikJunaid92@users.noreply.github.com>
2025-09-26 18:43:05 +0500

    Time: 4 ms (47.91%) | Memory: 19.2 MB (10.29%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.cpp
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.cpp
@@ -1,52 +1,21 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
-    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
-        ListNode dummy(0);
-        ListNode* current = &dummy;
-
-        while (l1 && l2) {
-            if (l1->val < l2->val) {
-                current->next = l1;
-                l1 = l1->next;
-            } else {
-                current->next = l2;
-                l2 = l2->next;
-            }
-            current = current->next;
-        }
-
-        // Attach the remaining part
-        current->next = (l1) ? l1 : l2;
-
-        return dummy.next;
-    }
-
     ListNode* mergeKLists(vector<ListNode*>& lists) {
-        if (lists.empty()) return nullptr;
-
-        int n = lists.size();
-        while (n > 1) {
-            int j = 0;
-            for (int i = 0; i < n / 2; ++i) {
-                lists[i] = mergeTwoLists(lists[j], lists[j + 1]);
-                j += 2;
+        vector<int> li;
+        for (auto list : lists) {
+            while (list != nullptr) {
+                li.push_back(list->val);
+                list = list->next;
             }
-            if (n % 2 == 1) {
-                lists[n / 2] = lists[n - 1];
-            }
-            n = (n + 1) / 2;
         }
 
-        return lists[0];
+        sort(li.begin(), li.end());
+        ListNode dummy(0);
+        ListNode* curr=&dummy;
+        for(int val : li){
+            curr->next = new ListNode(val);
+            curr= curr->next;
+        }
+        return dummy.next;
     }
 };

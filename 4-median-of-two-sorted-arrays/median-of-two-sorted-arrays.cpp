cd7d5d321c66a1b34db5c833e188db42b2b0e0e0
Muhammad Junaid Raza <114643027+MalikJunaid92@users.noreply.github.com>
2025-11-19 12:21:05 +0500

    Time: 11 ms (5.33%) | Memory: 96 MB (19.16%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
@@ -2,24 +2,22 @@ class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
         vector<int> arr;
-        
+
         for (int num : nums1) {
             arr.push_back(num);
         }
         for (int num : nums2) {
             arr.push_back(num);
         }
-        
+
         sort(arr.begin(), arr.end());
-        
+
         int n = arr.size();
-        
+
         if (n % 2 == 1) {
-            // odd length → return middle element
             return arr[n / 2];
-        } else {
-            // even length → return average of two middle elements
-            return (arr[n / 2 - 1] + arr[n / 2]) / 2.0;
+        }else{
+            return (arr[n/2 -1] + arr[n/2])/2.0;
         }
     }
 };

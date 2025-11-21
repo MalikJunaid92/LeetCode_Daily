4045614b6dccd0d2244bc6811e53a2bfad1a27f9
Muhammad Junaid Raza <114643027+MalikJunaid92@users.noreply.github.com>
2025-11-21 17:23:54 +0500

    Time: 0 ms (100.00%) | Memory: 23 MB (56.67%) - LeetSync

--- 110-balanced-binary-tree/balanced-binary-tree.cpp
+++ 110-balanced-binary-tree/balanced-binary-tree.cpp
@@ -1,18 +1,20 @@
 class Solution {
 public:
+    int solve(TreeNode* root) {
+        if (root == NULL) return 0;
+
+        int left = solve(root->left);
+        if (left == -1) return -1;  // left subtree unbalanced
+
+        int right = solve(root->right);
+        if (right == -1) return -1; // right subtree unbalanced
+
+        if (abs(left - right) > 1) return -1; // current node unbalanced
+
+        return max(left, right) + 1; // return height
+    }
+
     bool isBalanced(TreeNode* root) {
-        if(!root) return true;
-        int left= height(root->left);
-        int right= height(root->right);
-        if(abs(left - right) > 1) return false;
-        return isBalanced(root->left) && isBalanced(root->right);
+        return solve(root) != -1; // if -1 → unbalanced, else balanced
     }
-    
-    int height(TreeNode* root){
-        if(root == NULL){
-            return 0;
-        }
-        return 1+ max(height(root->left),height(root->right));
-    }
-    
 };
